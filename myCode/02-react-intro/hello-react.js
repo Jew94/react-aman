fbf313dd476d046817d7832cba34d5c62062631d
Ariel Rosenfeld <ariel.rosenfeld@asperii.com>
2020-03-04 08:29:16 +0200

    Added code from lesson 2

---
+++ myCode/02-react-intro/hello-react.js
@@ -0,0 +1,2 @@
+const h1=React.createElement('h1', null, 'React h1');
+ReactDOM.render(h1, document.getElementById('root'));
---
+++ myCode/02-react-intro/hello.js
@@ -0,0 +1,3 @@
+const h1=document.createElement('h1');
+h1.innerText='JS h1';
+document.getElementById('root').appendChild(h1);
